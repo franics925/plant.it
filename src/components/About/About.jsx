4ee49b4937a8989c57fb9b57fa8693a91df17a09
Gregory Redig <gregoryredig@Gregorys-MBP.attlocal.net>
2019-09-28 13:04:03 -0700

    modify layout of search and user elements

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './About.module.css';
+
+const About = (props) => (
+    <div className={style.About}>
+        About this app
+    </div>
+)
+
+export default About;
--- src/components/Search/SearchResults/SearchResults.js
+++ src/components/Search/SearchResults/SearchResults.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import style from './SearchResults.module.css';
+import About from '../../About/About';
 
 const SearchResults = (props) => (
     <div className={style.SearchResults}>
         SearchResults
+        < About />
     </div>
 )
 
