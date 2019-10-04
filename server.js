41ec95929af917f42c2166dfd6931dfd649d4df4
Gregory Redig <gregoryredig@Gregorys-MBP.attlocal.net>
2019-10-04 15:37:02 -0700

    configure express

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express')();
+const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
@@ -8,18 +8,34 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 
+//Used to submit forms, not needed for SPA
+// app.use(express.urlencoded());
+
 
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-const dotenv = require('dotenv');
-dotenv.config();
-const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello World'));
-app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
+//Ensure that api routes are mounted before "catch all" route
+
+//"catch all" route
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
+  console.log(`Express app running on port ${port}`);
 });
+
+// app.listen(port, () => console.log(`Server connected on port ${PORT}`));
+
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const PORT = process.env.PORT || 3000;
+// app.get('/', (req, res) => res.send('Hello World'));
+
+
+// app.listen(port, function() {
+//   console.log(`Express app running on port ${port}`)
+// });
