3e57ec376106f1539a21e2eef9ca19442b9d9aee
Gregory Redig <gregoryredig@Gregorys-MBP.attlocal.net>
2019-10-05 16:55:48 -0700

    create user model

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    name: String;
+    email: {type: String, required: true, lowercase: true, unique: true},
+    password: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
