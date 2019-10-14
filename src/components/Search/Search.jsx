9af8e56fa437aab9be53aae00d71517497bd8570
Gregory Redig <gregoryredig@Gregorys-MBP.attlocal.net>
2019-10-14 00:52:00 -0700

    display about page when no search results present

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PlantDetails from './PlantDetails/PlantDetails';
 import SearchResults from './SearchResults/SearchResults';
 import style from './Search.module.css';
+import About from '../About/About';
 
 
 
@@ -9,30 +10,29 @@ class Search extends Component {
     render() {
         return (
             <div className={style.Search}>
-                <div className={style.SearchPanels}>
-
-                    {this.props.resultsLoaded &&
+                {this.props.resultsLoaded ? 
+                    <div className={style.SearchPanels}>
                         <div className={style.SearchPanelLeft}>
-                            {this.props.resultsLoaded &&
                             < SearchResults
                                 handlePlantDetails={this.props.handlePlantDetails}
                                 searchResults={this.props.searchResults} 
                                 searchLoading={this.props.searchLoading}
                                 {...this.props}
-                            />}
+                            />
                         </div>
-                    }
 
-                    {this.props.plantSelectedId && 
-                        < PlantDetails
-                            className={style.SearchPanelRight}
-                            plantSelectedId={this.props.plantSelectedId}
-                            plantSelectedDetails={this.props.plantSelectedDetails}
-                            plantDetailsLoading={this.props.plantDetailsLoading}
-                        />
-                    }
-                    
-                </div>
+                        {this.props.plantSelectedId && 
+                            < PlantDetails
+                                className={style.SearchPanelRight}
+                                plantSelectedId={this.props.plantSelectedId}
+                                plantSelectedDetails={this.props.plantSelectedDetails}
+                                plantDetailsLoading={this.props.plantDetailsLoading}
+                            />
+                        }
+                        
+                    </div> 
+                    : < About />
+                }
             </div>
         )
     };
